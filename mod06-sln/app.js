ee5e1e906340f583b1fabdb18cd814f9818e1bb7
Ethan Wilansky <ewilansky@yahoo.com>
2017-10-05 19:37:23 -0400

    Final work on assignment 6

--- mod06-sln/app.js
+++ mod06-sln/app.js
@@ -1,35 +1,65 @@
 (function () {
 'use strict';
 
-angular.module('NameCalculator', [])
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('NameCalculatorController', function ($scope) {
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController ($scope) {
   $scope.foodList = "";
   $scope.foodMsg = "";
+  $scope.noteMsg = "";
+  $scope.customStyle = {};
 
   $scope.displayMessage = function () {
     var foodMsgValue = displayMessageForString($scope.foodList);
     $scope.foodMsg = foodMsgValue;
   };
 
+  $scope.turnGreen = function() {
+    $scope.customStyle.colorClass = 'box green';
+  };
+
+  $scope.turnRed = function() {
+    $scope.customStyle.colorClass = 'box red';
+  }
+
   function displayMessageForString(string) {
     var whiteSpaceRemoved = string.replace(/\s/g,'');
-    var multipleCommas = whiteSpaceRemoved.indexOf(',,');
-
-    if (string === '') {
-      return 'Please enter data first.';
-   } else if (multipleCommas != -1) {
-       return 'Sorry, multiple commas in a row are not allowed.';
+    var repeatingCommas = whiteSpaceRemoved.indexOf(',,');
+    var cleanString = '';
+    
+    // clean-up white space
+    cleanString = repeatingCommas != -1 ? whiteSpaceRemoved.replace(/[,\s]{2,}/,"") : whiteSpaceRemoved;
+        
+    // detect, report and remove repeating commas
+    if (repeatingCommas != -1) {
+      cleanString = whiteSpaceRemoved.replace(/[,\s]{2,}/,",");
+      $scope.noteMsg = "Multiple commas in a row do not count as lunch items."
     } else {
-      var items = string.split(',');
+      cleanString = whiteSpaceRemoved;
+      $scope.noteMsg = "";
+    }
+
+    // final cleaning to remove leading or trailing commas
+    cleanString = cleanString.replace(/(^,)|(,$)/g, "");
+      
+    if (cleanString === '') {
+      $scope.turnRed();
+      // $scope.noteMsg = "Not much of a list quite yet."
+      return 'Please enter data first.';
+    } else {
+      var items = cleanString.split(',');
       if (items.length <= 3) {
+        $scope.turnGreen();
         return 'Enjoy!';
       } else {
+        $scope.turnGreen();
         return 'Too much!';
       }  
     }
-  }
-
-});
+  };
+}
 
 })();
