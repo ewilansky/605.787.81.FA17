23e3d0c9128314b173dcf81dd0d49cd443659a2a
Ethan Wilansky <ewilansky@yahoo.com>
2017-11-16 19:00:45 -0500

    Minor style updates and clean-up

--- mod09-sln/src/menuapp/data.module.js
+++ mod09-sln/src/menuapp/data.module.js
@@ -2,6 +2,6 @@
     'use strict';
     
     // create module Data - depends on ui.router
-    angular.module('Data', ['ui.router', 'Spinner']);
+    angular.module('Data', ['ui.router']);
     
     })();
