7ca30a64522f4a74248519efd0e8712d9bfa4564
Ethan Wilansky <ewilansky@yahoo.com>
2017-12-05 17:55:37 -0500

    Working form via ui-router

---
+++ mod10-sln/src/public/public.routes.js
@@ -0,0 +1,62 @@
+(function() {
+'use strict';
+
+angular.module('public')
+.config(routeConfig);
+
+/**
+ * Configures the routes and views
+ */
+routeConfig.$inject = ['$stateProvider'];
+function routeConfig ($stateProvider) {
+  // Routes
+  $stateProvider
+    .state('public', {
+      abstract: true,
+      templateUrl: 'src/public/public.html'
+    })
+    .state('public.home', {
+      url: '/',
+      templateUrl: 'src/public/home/home.html'
+    })
+    .state('public.menu', {
+      url: '/menu',
+      templateUrl: 'src/public/menu/menu.html',
+      controller: 'MenuController',
+      controllerAs: 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService', function (MenuService) {
+          return MenuService.getCategories();
+        }]
+      }
+    })
+    .state('public.menuitems', {
+      url: '/menu/',
+      templateUrl: 'src/public/menu-items/menu-items.html',
+      controller: 'MenuItemsController',
+      controllerAs: 'menuItemsCtrl',
+      resolve: {
+        menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
+    })
+    .state('public.register', {
+      url: '/register/',
+      params: {
+        firstName: null,
+        lastName: null,
+        email: null,
+        phone: null
+      },
+      templateUrl: 'src/public/register/register.html',
+      controller: 'RegistrationController',
+      controllerAs: 'regCtrl',
+      resolve: {
+        userData: ['$stateParams', function($stateParams) {
+          console.log($stateParams.table);
+        }]
+      }
+    });
+}
+})();
